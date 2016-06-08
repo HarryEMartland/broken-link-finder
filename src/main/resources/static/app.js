277b11fdbd294f0e7559e017bcee6a5e151e0cb8
harryemartland <harryemartland@gmail.com>
2016-06-08 19:45:36 +0100

    hide loading message on error

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -23,6 +23,7 @@ $(function(){
 
                     function showError(error){
                         $('.hideUntilSearch').hide();
+                        $('.showLoading').hide();
                         $('#errorWrapper').append('<div class="alert alert-danger alert-dismissible" role="alert">'+
                                                      '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
                                                      '<span aria-hidden="true">&times;</span></button>'+
